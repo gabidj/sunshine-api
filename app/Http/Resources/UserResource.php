4c7056f10712acb725f9f4cfe76ff113530bd467
Gabi Suciu <git@gabisuciu.ro>
2022-04-18 19:06:25 +0300

    Resource examples

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -8,16 +8,56 @@ use OpenApi\Annotations as OA;
 /**
  * @OA\Info(title="My First API", version="0.1")
  * @OA\PathItem (path="/api/user")
+ * @OA\Schema()
  */
 class UserResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
      *
-    /**
-     * OA\Get(
-     *     path="/api/resource.json",
-     *     @OA\Response(response="200", description="An example resource")
+     *
+     * /**
+     * @OA\Post(
+     *     path="/users",
+     *     summary="Adds a new user - with oneOf examples",
+     *     description="Adds a new user",
+     *     operationId="addUser",
+     *     tags={"user"},
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="id",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="name",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="phone",
+     *                     oneOf={
+     *                     	   @OA\Schema(type="string"),
+     *                     	   @OA\Schema(type="integer"),
+     *                     }
+     *                 ),
+     *                 example={"id": "a3fb6", "name": "Jessica Smith", "phone": 12345678}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK",
+     *         @OA\JsonContent(
+     *             oneOf={
+     *                 @OA\Schema(ref="#/components/schemas/User"),
+     *                 @OA\Schema(type="boolean")
+     *             },
+     *             @OA\Examples(example="result", value={"success": true}, summary="An result object."),
+     *             @OA\Examples(example="bool", value=false, summary="A boolean value."),
+     *         )
+     *     )
      * )
      *
      * @param  \Illuminate\Http\Request  $request
