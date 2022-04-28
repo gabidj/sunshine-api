c95f1e8c78cfdbd9011798463b27943b57628fc4
Gabi Suciu <gabriel.suciu@tss-yonder.com>
2022-04-28 14:58:45 +0300

    Add oauth & external docs

--- app/Http/OpenApiSpec/OpenApiSpec.php
+++ app/Http/OpenApiSpec/OpenApiSpec.php
@@ -8,7 +8,35 @@ use OpenApi\Annotations as OA;
 // These should only appear once in the API
 /**
  * @OA\Info(title="My First API", version="0.1")
- * @OA\PathItem (path="/api/user")
+ * @OA\PathItem (path="/api")
+ * @OA\Server(
+ *     description="SwaggerHUB API Mocking",
+ *     url="https://virtserver.swaggerhub.com/swagger/Petstore/1.0.0"
+ * )
+ * @OA\ExternalDocumentation(
+ *     description="Find out more EcoOnline",
+ *     url="http://ecoonline.com"
+ * )
+ *
+ * @OA\SecurityScheme(
+ *     type="oauth2",
+ *     name="sunshine_auth",
+ *     securityScheme="sunshine_auth",
+ *     @OA\Flow(
+ *         flow="implicit",
+ *         authorizationUrl="https://sunshine-api.test/oauth/dialog",
+ *         scopes={
+ *             "write:users": "modify users in your account",
+ *             "read:users": "read your users",
+ *         }
+ *     )
+ * )
+ * @OA\SecurityScheme(
+ *     type="apiKey",
+ *     in="header",
+ *     securityScheme="api_key",
+ *     name="api_key"
+ * )
  */
 class OpenApiSpec
 {
